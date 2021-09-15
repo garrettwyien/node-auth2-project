cad8b3726f708c225187aca05717be010ece9a40
Garrett Yien <yien.garrett94@gmail.com>
2021-09-15 02:43:42 -0400

    built role and token middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,23 @@
+const { default: jwtDecode } = require("jwt-decode");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 
 const restricted = (req, res, next) => {
+  const token = req.headers.authorization
+  if (!token) return next({
+    status: 401, message: 'Token required'
+  })
+
+  jwtDecode.verify(
+    token,
+    JWT_SECRET,
+    (err, decoded) => {
+      if (err) return next({
+        status:401, message: "Token invalid", errorMessage: err.message
+      })
+      req.decodedJwt = decoded
+      next()
+    }
+  )
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -19,6 +36,14 @@ const restricted = (req, res, next) => {
 }
 
 const only = role_name => (req, res, next) => {
+  const { decodedJwt } = req
+  if (decodedJwt.role_name === role_name) {
+    next()
+  } else {
+    next({
+      status:403, message: "This is not for you"
+    })
+  }
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
